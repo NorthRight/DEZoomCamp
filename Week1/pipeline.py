dd71258bfedf12729c02552d54f98446ff7f7fb5
Thomas <thomashowe456@gmail.com>
2023-01-21 22:01:45 +1300

    addingfirstdockerfile

---
+++ Week1/pipeline.py
@@ -0,0 +1,10 @@
+# from https://www.youtube.com/watch?v=EYNwNlOrpr0&list=PL3MmuxUbc_hJed7dXYoJw8DoCuVHhGEQb
+import pandas as pd
+import sys
+
+print(sys.argv)
+
+day = sys.argv[1]
+# A python pandas test
+
+print(f"This is a test script using Docker, This is being run for {day}")
